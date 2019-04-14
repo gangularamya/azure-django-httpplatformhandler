bd1187caac754bb52c31397826b71334bb82471a
Ramya Gangula <ramya1331@gmail.com>
2019-04-13 20:05:08 -0500

    removed sendgrid code and included / route

--- polls/views.py
+++ polls/views.py
@@ -1,10 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.core.mail import send_mail
 
 
 def index(request):
-    send_mail('Subject here', 'Here is the message.', 'ramya.gangula@microsoft.com', ['ramya.gangula@microsoft.com'], fail_silently=False)
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("Hello, world.")
 
 # Create your views here.
